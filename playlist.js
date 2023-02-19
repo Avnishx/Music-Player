1f34f9bfcfdc3b52ec8a7a698d15c6630512fbf6
Avnish Yadav <112178009+Avnishx@users.noreply.github.com>
2023-02-19 14:19:02 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -12,16 +12,106 @@ let songs = [
     audio : "music2"
    }, 
    { 
-    name : "Slipping Away - Dyalla" ,
-    artist : "Dyalla",
+    name : "Sigma song - Patrick" ,
+    artist : "Patrick Bateman",
     img : "img3",
     audio : "music3"
    }, 
    { 
-    name : "Desert Planet - Quincas" ,
-    artist : "Quincas",
+    name : "The Perfect Phonk" ,
+    artist : "Lorean",
     img : "img4",
     audio : "music4"
    }, 
+   { 
+      name : "Sign For The Moment" ,
+      artist : "Eminem",
+      img : "img5",
+      audio : "music5"
+     }, 
+     { 
+      name : "Baller" ,
+      artist : "Shubh, Ikky",
+      img : "img6",
+      audio : "music6"
+     }, 
+     { 
+      name : "Dil Nu" ,
+      artist : "AP Dillon, Shinda Kahlon",
+      img : "img7",
+      audio : "music7"
+     }, 
+     { 
+      name : "Insaan" ,
+      artist : "AP, Shinda, Gurinder",
+      img : "img8",
+      audio : "music8"
+     }, 
+     { 
+      name : "Elevated" ,
+      artist : "Shubh",
+      img : "img9",
+      audio : "music9"
+     }, 
+     { 
+      name : "Toxic" ,
+      artist : "AP Dillon, Intense",
+      img : "img10",
+      audio : "music10"
+     }, 
+     { 
+      name : "Summer High" ,
+      artist : "AP Dillon",
+      img : "img11",
+      audio : "music11"
+     }, 
+     { 
+      name : "Bombay to Punjab" ,
+      artist : "Deep Jandu, DIVINE",
+      img : "img13",
+      audio : "music13"
+     }, 
+     { 
+      name : "Maiyya Mainu" ,
+      artist : "Sachet Tandon",
+      img : "img14",
+      audio : "music14"
+     }, 
+     { 
+      name : "Kaleshi Chori" ,
+      artist : "DG IMMORTALS,RAGA,Harjas",
+      img : "img15",
+      audio : "music15"
+     }, 
+     { 
+      name : "Gang wale munde" ,
+      artist : "Paradox",
+      img : "img16",
+      audio : "music16"
+     }, 
+     { 
+      name : "Bandeya" ,
+      artist : "Arijit Singh, Shaarib Toshi",
+      img : "img17",
+      audio : "music17"
+     }, 
+     { 
+      name : "Aise Kyun" ,
+      artist : "Raghav Chaitanya",
+      img : "img18",
+      audio : "music18"
+     }, 
+     { 
+      name : "Phir Le Aya Dil" ,
+      artist : "Arijit Singh",
+      img : "img19",
+      audio : "music19"
+     }, 
+     { 
+      name : "Hawayein" ,
+      artist : "VIBIE, Arijit Singh",
+      img : "img20",
+      audio : "music20"
+     }, 
 
 ] 
